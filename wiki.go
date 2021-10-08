a65022960858a66be4eb3d0515c07f1d258c58a8
philFernandez <norcal618@gmail.com>
2021-10-07 23:48:25 -0700

    handle errors in save

--- wiki.go
+++ wiki.go
@@ -63,7 +63,11 @@ func main() {
 		title := r.URL.Path[len("/save/"):]
 		body := r.FormValue("body")
 		p := &Page{Title: title, Body: []byte(body)}
-		p.save()
+		err := p.save()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		http.Redirect(w, r, "/view/"+title, http.StatusFound)
 	})
 	const portNumber = ":8080"
