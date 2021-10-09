0b8abf9390ae7ee30ae910573a6fff2ded193de6
philFernandez <norcal618@gmail.com>
2021-10-08 20:24:04 -0700

    makeHandler wrapper

--- wiki.go
+++ wiki.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -37,13 +36,15 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
 	}
-	return m[2], nil
 }
 
 func render(w http.ResponseWriter, templateFile string, p *Page) {
@@ -54,44 +55,31 @@ func render(w http.ResponseWriter, templateFile string, p *Page) {
 }
 
 func main() {
-	http.HandleFunc("/view/", func(w http.ResponseWriter, r *http.Request) {
-		title, err := getTitle(w, r)
-		if err != nil {
-			return
-		}
+	http.HandleFunc("/view/", makeHandler(func(w http.ResponseWriter, r *http.Request, title string) {
 		p, err := loadPage(title)
 		if err != nil {
 			http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 			return
 		}
 		render(w, "view", p)
-	})
-	http.HandleFunc("/edit/", func(w http.ResponseWriter, r *http.Request) {
-		title, err := getTitle(w, r)
-		if err != nil {
-			return
-		}
+	}))
+	http.HandleFunc("/edit/", makeHandler(func(w http.ResponseWriter, r *http.Request, title string) {
 		p, err := loadPage(title)
 		if err != nil {
 			p = &Page{Title: title}
 		}
 		render(w, "edit", p)
-
-	})
-	http.HandleFunc("/save/", func(w http.ResponseWriter, r *http.Request) {
-		title, err := getTitle(w, r)
-		if err != nil {
-			return
-		}
+	}))
+	http.HandleFunc("/save/", makeHandler(func(w http.ResponseWriter, r *http.Request, title string) {
 		body := r.FormValue("body")
 		p := &Page{Title: title, Body: []byte(body)}
-		err = p.save()
+		err := p.save()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		http.Redirect(w, r, "/view/"+title, http.StatusFound)
-	})
+	}))
 	const portNumber = ":8080"
 	fmt.Println("Server Started on http://localhost" + portNumber)
 	log.Fatal(http.ListenAndServe(portNumber, nil))
